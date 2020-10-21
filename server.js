55d194ce96d63aa46c9dfad1d2e9e2991090a60e
mfarid99 <67521156+mfarid99@users.noreply.github.com>
2020-10-21 00:24:52 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+require ("dotenv").config();
+const express = require("express");
+const app = express();
+const {PORT = 3000} = process.env;
+const cors = require ("cors");
+const morgan = require ("morgan");
+const mongoose = require ("./db/db");
+const AuthRouter = require("./controllers/user")
+const auth = require("./auth")
+
+//Middleware
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan("tiny"))
+
+//Routers
+app.get("/", auth, (req, res) => {
+    res.json(req.payload)
+});
+
+app.use("/auth", AuthRouter)
+
+//Listeners
+
+app.listen(PORT, () => {
+    console.log(`you are listening on port ${PORT}`)
+});
